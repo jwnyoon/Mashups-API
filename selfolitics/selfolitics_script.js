b92773700dfc380ce160e68d4c6e5733ab6bdc34
Jiwon Yoon <jiwon.yoon8@gmail.com>
2015-03-11 00:36:29 -0400

    Update selfolitics_script.js

--- selfolitics/selfolitics_script.js
+++ selfolitics/selfolitics_script.js
@@ -9,12 +9,10 @@ this.img="";
 
 var instaToday =[];
 
-
-
 function getTodayData(curSearchTerm){
 	var TodaySearchURL ='http://api.usatoday.com/open/articles?section=politics&keyword='
     var searchTerm = curSearchTerm;
-    var myTodayKey='&encoding=json&api_key=vc6hja3hrmrhh7xrg9f4xxsn';
+    var myTodayKey='YOUR_TODAY_API_KEY';
 
 	$.ajax({
 		url: TodaySearchURL + searchTerm + myTodayKey,
@@ -22,20 +20,13 @@ function getTodayData(curSearchTerm){
 		dataType: 'json',
 		error: function(data){
 			console.log("We got problems");
-			//console.log(data);
-			//console.log(data.status);
-
-			console.log("Making Request with safe word");
-			//var safeWord = 'politics';
-			//getTodayData(safeWord);
 			alert("You entered " + curSearchTerm + "??? Please try entering a different word.");
-			//$('body').append('<div>SORRY!!!!</div>');
 		},
 		success: function(data){
 			console.log("WooHoo!");
 			var TodayArticles;
 			if(!(data.stories instanceof Array)){
-				console.log("Huh?? Data is not an array");
+				console.log("Data is not an array");
 				return;
 			}
 			else{
@@ -63,9 +54,8 @@ function getTodayData(curSearchTerm){
 function getInstagramData(){
 	"use strict";
 	var curTag = 'selfie';
-	var myInstaKey = 'a934e4db280f4cdbb14a19e683560ea0';
+	var myInstaKey = 'YOUR_INSTAGRAM_API_KEY';
 	var instagramTagSearchURL = 'https://api.instagram.com/v1/tags/' + curTag + '/media/recent?client_id=' + myInstaKey;
-	//var instagramPopularSearchURL = 'https://api.instagram.com/v1/media/popular?client_id=' + myInstaKey;
 
 	$.ajax({
 		url: instagramTagSearchURL,
@@ -120,10 +110,6 @@ $(document).ready(function(){
 	$("#inputBox").focus(function() {
 		$("#inputBox").val("");
 	});
-
-	//$("#inputBox").focusout(function() {
-		//$("#inputBox").val("Hey, where did you go?");
-	//});
 
 	$("#update").click(function(){
 		console.log("Clicked Update");
